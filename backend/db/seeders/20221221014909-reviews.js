fc4efecd11df2ac18efded39decf7af2ed2078dd
Christian Lee <christian.leehk@gmail.com>
2023-01-02 18:08:53 -0800

    refactor seeds

--- backend/db/seeders/20221221014909-reviews.js
+++ backend/db/seeders/20221221014909-reviews.js
@@ -45,7 +45,7 @@ module.exports = {
     options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2, 3] }
+      spotId: { [Op.in]: [1, 2, 3, 4, 5] }
     }, {});
   }
 };
--- backend/db/seeders/20221221031653-spot-images.js
+++ backend/db/seeders/20221221031653-spot-images.js
@@ -46,7 +46,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2, 3] }
+      spotId: { [Op.in]: [1, 2, 3, 4] }
     }, {});
   }
 };
