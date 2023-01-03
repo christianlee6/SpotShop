cba59d7af997c8c69a4d7bd0f1f73ccf7bca94e2
Christian Lee <christian.leehk@gmail.com>
2023-01-02 17:46:48 -0800

    resolve merge conflict in users.js/41

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -38,45 +38,6 @@ router.post("/", async (req, res) => {
 
     if (!lastName) {
         errObj.errors.lastName = "Last Name is required";
-=======
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
-
-const validateSignup = [
-    check('email')
-      .exists({ checkFalsy: true })
-      .isEmail()
-      .withMessage('Please provide a valid email.'),
-    check('username')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 4 })
-      .withMessage('Please provide a username with at least 4 characters.'),
-    check('username')
-      .not()
-      .isEmail()
-      .withMessage('Username cannot be an email.'),
-    check('password')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 6 })
-      .withMessage('Password must be 6 characters or more.'),
-    handleValidationErrors
-  ];
-
-// Sign up
-router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
-      const { email, password, username, firstName, lastName } = req.body;
-      const user = await User.signup({ email, username, password, firstName, lastName });
-
-      await setTokenCookie(res, user);
-
-      return res.json({
-        user: user
-      });
     }
 
     if (!email || !username || username.length < 5 || !firstName || !lastName || !email.includes("@")) {
@@ -134,8 +95,6 @@ router.post(
         }
     });
 
-
-
     const user = await User.signup({
         email,
         username,
@@ -147,6 +106,29 @@ router.post(
     await setTokenCookie(res, user);
 
     return res.json(user);
-});
+
+})
+
+// const validateSignup = [
+//     check('email')
+//       .exists({ checkFalsy: true })
+//       .isEmail()
+//       .withMessage('Please provide a valid email.'),
+//     check('username')
+//       .exists({ checkFalsy: true })
+//       .isLength({ min: 4 })
+//       .withMessage('Please provide a username with at least 4 characters.'),
+//     check('username')
+//       .not()
+//       .isEmail()
+//       .withMessage('Username cannot be an email.'),
+//     check('password')
+//       .exists({ checkFalsy: true })
+//       .isLength({ min: 6 })
+//       .withMessage('Password must be 6 characters or more.'),
+//     handleValidationErrors
+//   ];
+
+
 
 module.exports = router;
